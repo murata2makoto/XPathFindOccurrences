3591131cbe66c1a41417873cf71c13ed9691bbdd
murata2makoto <eb2mmrt@gmail.com>
2022-10-08 12:18:59 +0900

    Fixed a bug in mergesort

--- XPathFindOccurrences/MergeSort.fs
+++ XPathFindOccurrences/MergeSort.fs
@@ -3,8 +3,8 @@
 let mergesort1 (comparer : 'a -> 'a -> int)
                 (l1: 'a list) (l2: 'a list) =
     let rec help toBePrepended (l1: 'a list) (l2: 'a list) =
-        if l1.IsEmpty then List.append l2 toBePrepended 
-        elif l2.IsEmpty then List.append l1 toBePrepended 
+        if l1.IsEmpty then List.append (List.rev l2) toBePrepended 
+        elif l2.IsEmpty then List.append (List.rev l1) toBePrepended 
         else 
           let h1 = List.head l1
           let h2 = List.head l2
