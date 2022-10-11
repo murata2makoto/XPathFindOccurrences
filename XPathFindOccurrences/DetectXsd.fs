867f9c61f68503c46d5ad70d3438af04805ae541
murata2makoto <eb2mmrt@gmail.com>
2022-10-11 11:51:12 +0900

    Detect references to schemas

---
+++ XPathFindOccurrences/DetectXsd.fs
@@ -0,0 +1,8 @@
+﻿module XPathFindOccurrences.DetectXsd
+
+let detectXsd (contents: string seq): bool = 
+    if Seq.isEmpty contents then 
+        failwith "empty contents"
+    else
+        let firstContent = Seq.head contents
+        firstContent.Contains("W3C XML Schema definition")
--- XPathFindOccurrences/Program.fs
+++ XPathFindOccurrences/Program.fs
@@ -2,14 +2,18 @@
 
 module XPathFindOccurrences.Program
 
+let private ignoreCase = System.StringComparison.CurrentCultureIgnoreCase
+
 open NERGList
 open Toolkit
 open CreateSectionFinder
 open GetStartEndMarkerPairs
 open ForbiddenWords
+open DetectXsd
 open System.IO
 open System.Xml.Linq
 
+type Nerg = Note | Example | Rationale | Guidance;;
 
 let readXPaths inputFileName = 
     let enc = new System.Text.UTF8Encoding(true)
@@ -22,25 +26,46 @@ let readXPaths inputFileName =
     else failwith "Specify two XPath expressions"
 
 
-let help2 (pairs: (string * 
+let help2 nerg (pairs: (string * 
                     XElement * 
                     XElement * seq<string>) list)
           finder  sw  = 
-    fprintfn sw "%s\t%s\t%s\t%s\t%s"
-        "Subclasue number"
-        "Presense of normative modal verbs" 
-        "Start Marker Position" 
-        "End Marker Position"  
-        "Content"    
+    if nerg = Note then
+        fprintfn sw "%s\t%s\t%s\t%s\t%s\t%s"
+            "Subclasue number"
+            "Presense of normative modal verbs" 
+            "Reference to schemas" 
+            "Start Marker Position" 
+            "End Marker Position"  
+            "Content" 
+    else
+        fprintfn sw "%s\t%s\t%s\t%s\t%s"
+            "Subclasue number"
+            "Presense of normative modal verbs" 
+            "Start Marker Position" 
+            "End Marker Position"  
+            "Content" 
     for (xpath, startElem, _ , contents) in pairs do 
         let sectionNumber = finder startElem
         let msp_mep_pairs = 
             getStartEndMarkerPairs xpath contents
         for (msp, mep) in msp_mep_pairs do
-            let flag = hasForbiddenWords contents msp mep 
-            fprintf sw "%s\t%b\t%d\t%d\t" sectionNumber flag msp mep 
+            let forbiddenWordsFlag = hasForbiddenWords contents msp mep 
+            let xsdFlag = detectXsd contents
+            if nerg = Note then
+                fprintf sw "%s\t%b\t%b\t%d\t%d\t" 
+                    sectionNumber forbiddenWordsFlag xsdFlag msp mep 
+            else
+                fprintf sw "%s\t%b\t%d\t%d\t" sectionNumber forbiddenWordsFlag msp mep 
             contents |> Seq.iter (fun cont -> fprintf sw "%s " cont) 
             sw.WriteLine()
+
+let detectNerg (xpath: string) = 
+    if xpath.Contains("Example",ignoreCase) then Example
+    elif xpath.Contains("Note", ignoreCase) then Note
+    elif xpath.Contains("Guidance", ignoreCase) then Guidance
+    elif xpath.Contains("Rationale", ignoreCase) then Rationale
+    else failwith "unrecognized nerg"
 
 [<EntryPoint>]
 let main argv =
@@ -50,14 +75,14 @@ let main argv =
             let doc = createXDocumentFromDocxFileName docXFileName
             let mgr = getManager doc
             let part1P = docXFileName.Contains("29500-1")
-            //let hash = createHashContainingParagarphsAndTableRows doc mgr
+            let nerg = xPaths.Head |> detectNerg 
             let sectionFinder = 
                 createSectionFinder 
                     (createTitleElemList doc mgr part1P)
             let nergList = 
                 extractNERGList xPaths doc mgr 
             use sw = createTextWriteFromOutputFileName outputFileName
-            help2 nergList sectionFinder sw 
+            help2 nerg nergList sectionFinder sw 
             sw.Close()
             0
     | _ -> 
