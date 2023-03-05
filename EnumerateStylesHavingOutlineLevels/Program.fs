73fd925bce550446707ef52fc8241ace0d9b3059
murata2makoto <eb2mmrt@gmail.com>
2023-03-05 20:55:32 +0900

    Created a new project

---
+++ EnumerateStylesHavingOutlineLevels/Program.fs
@@ -0,0 +1,26 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open OOXML.Toolkit
+open XPathFindOccurrences.SecTitle
+
+open System.Xml.XPath
+open System.Xml.Linq
+open System.IO
+open System.IO.Compression
+
+let tocStylesXPath = "//w:style[.//w:outlineLvl]/w:name"
+
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| docXFileName |] ->
+            let doc = createXDocumentFromDocxFileName docXFileName
+                        "\\word\\styles.xml"
+            let mgr = getManager doc
+            for tocStyle in doc.XPathSelectElements(tocStylesXPath, mgr) do
+                let valAtt = tocStyle.Attribute(wml + "val")
+                printfn "%s" valAtt.Value
+            0
+    | _ -> 
+        printfn "Illegal parameter %A" argv
+        1
